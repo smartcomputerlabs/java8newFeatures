46ccee0b86c0d45ba5cb26d6db44cb5568a7944f
smartcomputerlabs <46390666+smartcomputerlabs@users.noreply.github.com>
2021-10-17 17:23:30 +0530

    test

--- src/java8newFeatures/interfaces/Java8MultipleInheritance.java
+++ src/java8newFeatures/interfaces/Java8MultipleInheritance.java
@@ -1,44 +1,43 @@
 package java8newFeatures.interfaces;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.function.Consumer;
-
-public class Java8MultipleInheritance {
+//public class MulitpleInheritanceDemo implements A1, B1 { // compilation error implement doStuff
+//public class MulitpleInheritanceDemo implements A2, B1 { // compilation error implement doStuff as B1 doesn't override doStuff, but has it's pwn doStuff
+//	public class MulitpleInheritanceDemo implements A1, B2 { // allowed doStuff from B2 invoked, as doStuff of A1 is overridden in B2
+//	public class MulitpleInheritanceDemo implements A2, B2 { // allowed doStuff from B2 invoked, as doStuff of A2 is overridden in B2
+public class MulitpleInheritanceDemo implements A3 { // compilation error implement doStuff
 	public static void main(String[] args) {
+		new MulitpleInheritanceDemo().doStuff();
+	}
 
-		// create a sample Collection of strings
-		List<String> listOfStrings = new ArrayList<String>();
-		listOfStrings.add("First");listOfStrings.add("Second");listOfStrings.add("Third");
+	/*@Override
+	public void doStuff() {
+		// TODO Auto-generated method stub
+		B1.super.doStuff();
+	}*/
 
-		// lets traverse our listOfStrings using Iterator
-		System.out.println("-------- traversing our listOfStrings using Iterator------------------");
-		Iterator<String> strIterator = listOfStrings.iterator();
-		while (strIterator.hasNext()) {
-			String str = strIterator.next();
-			System.out.println("Iterator Value = " + str);
-		}
+}
 
-		// now traverse listOfStrings using java8 forEach method of Iterable with anonymous class
-		System.out.println("-------- traversing our listOfStrings using java8 forEach method of Iterable with anonymous class ---------");
-		listOfStrings.forEach(new Consumer<String>() {
-			public void accept(String t) {
-				System.out.println("forEach using anonymous class, Value = " + t);
-			}
-		});
-		
-		// now traverse listOfStrings with our Consumer interface implementation
-		System.out.println("-------- traversing our listOfStrings using our Consumer interface implementation --------------");
-		MyStringListConsumer action = new MyStringListConsumer();
-		listOfStrings.forEach(action);
+interface A1 {
+	default void doStuff() {
+		System.out.println("A1.doStuff()");
 	}
 }
 
-//This is Consumer implementation for String variable that can be reused
-class MyStringListConsumer implements Consumer<String>{
-	public void accept(String t) {
-		// Make any formatting changes here
-		System.out.println("Consumer impl Value::"+t);
+interface A2 extends A1 {
+}
+
+interface A3 extends A1 {
+	void doStuff();
+}
+
+interface B1 {
+	default void doStuff() { // line n1
+		System.out.println("B1.doStuff()");
+	}
+}
+
+interface B2 extends A1 {
+	default void doStuff() { // line n1
+		System.out.println("B1.doStuff()");
 	}
 }
