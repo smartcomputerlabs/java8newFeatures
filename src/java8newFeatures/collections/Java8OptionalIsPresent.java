0285ea031a9bfe9074ecb5a83b7454ee982a5702
smartcomputerlabs <46390666+smartcomputerlabs@users.noreply.github.com>
2021-10-19 13:05:45 +0530

    test

--- src/java8newFeatures/collections/Java8OptionalIsPresent.java
+++ src/java8newFeatures/collections/Java8OptionalIsPresent.java
@@ -2,7 +2,9 @@ package java8newFeatures.collections;
 
 import java.util.Optional;
 
-public class Java8OptionalIsPresent {
-    Optional<String> opt = Optional.of("NonEmptyString");
-
+public class Java8OptionalIsPresent<optn> {
+    void testOptional() {
+        Optional<String> optional = Optional.of("NonEmptyString");
+        System.out.println(optional.isPresent());
+    }
 }
